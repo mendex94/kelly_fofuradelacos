2869c140ff368ed9c67e84d663a948df08fa689b
Felipe Mendes <lfelipecmendes@icloud.com>
2022-06-29 19:28:42 -0300

    Minor fixes on the footer

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -11,26 +11,26 @@ function Footer() {
       <div className='flex gap-[63px]'>
         <div className='hidden md:inline mb-'>
           <LogoFooter />
-          <p className='text-p4'>&copy; 2022 | All Rights Reserved.</p>
+          <p className='text-p4 text-bege-100'>&copy; 2022 | All Rights Reserved.</p>
         </div>
         <div className='flex flex-col justify-center'>
          <div className='flex gap-[68px]'>
-         <div className='flex flex-col font-fiona text-h4'>
+         <div className='flex flex-col font-fiona text-h4 items-center md:items-start'>
             <a href="#">Sobre nós</a>
             <a href="#">Dúvidas</a>
           </div>
-          <div className='flex flex-col font-fiona'>
+          <div className='flex flex-col font-fiona items-center md:items-start'>
             <a href="#">Loja</a>
             <a href="#">Personalize</a>
           </div>
          </div>
-          <div className='flex gap-6 mt-16 text-h4'>
+          <div className='flex gap-6 mt-16 text-h4 justify-center md:justify-start'>
             <a href="#"><img src={InstagramIcon} alt="" /></a>
             <a href="#"><img src={WhatsAppIcon} alt="" /></a>
           </div>
         </div>
       </div>
-      <div className='font-fiona text-h4 text-right gap-4 flex-col'>
+      <div className='font-fiona text-h4 text-center md:text-right gap-4 flex-col'>
         <p>Certificados</p>
         <div className='flex gap-5'>
           <img className='w-[2.5rem] h-[2.5rem] md:w-full md:h-full' src={GenericBadge} alt="" />

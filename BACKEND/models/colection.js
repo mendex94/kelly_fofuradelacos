51d7fa550a526266265e3d02482788016dc43552
tadlana <100472925+tadlana@users.noreply.github.com>
2022-06-28 21:09:44 -0300

    feat(models): adicionando colection e color

--- BACKEND/models/colection.js
+++ BACKEND/models/colection.js
@@ -1,27 +1,34 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class colection extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+const db = require ('../dataBase/dbConexao')
+const { DataTypes } = require ('sequelize')
+
+const Colection = db.define ('colection', {
+    id_colection: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    date_begin: {
+        type: DataTypes.DATE
+    },
+    date_end: {
+        type: DataTypes.DATE
+    },
+    status: {
+        type: DataTypes.INTEGER
+    },
+    createdAt: {
+        type: DataTypes.DATE
+    },
+    updatedAt: {
+        type: DataTypes.DATE
     }
-  }
-  colection.init({
-    id_colection: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    date_begin: DataTypes.DATE,
-    date_end: DataTypes.DATE,
-    status: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'colection',
-  });
-  return colection;
-};
+}, {
+    tableName: 'colection'
+})
+
+module.exports = Colection
+

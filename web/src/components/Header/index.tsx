82349b7d7610924b278c42a6f08bb4b3c60d1180
Felipe Mendes <lfelipecmendes@icloud.com>
2022-06-29 20:45:06 -0300

    header fixed

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -15,13 +15,13 @@ function Header() {
   }
 
   return (
-    <header className='flex h-[150px] w-screen justify-start md:justify-around items-center font-fiona text-[32px] p-4 bg-[#F9F7F4]'>
+    <header className='flex h-[150px] w-screen justify-start md:justify-center items-center font-fiona text-[32px] p-4 bg-[#F9F7F4]'>
       <LogoHeader/>
       <nav className='flex'>
         <div className='absolute right-6 lg:hidden top-6 scale-150'>
            <MenuOutlined className='scale-150 cursor-pointer text-rosa-200' onClick={showMenu}/>
         </div>
-        <ul className='hidden lg:flex gap-30 p-10 text-[32px] text-preto-100'>
+        <ul className='hidden lg:flex gap-[60px] text-[32px] text-preto-100 px-32'>
           <li><a href="#">HOME</a></li>
           <li><a href="#">LOJA</a></li>
           <li><a href="#">SOBRE NÓS</a></li>

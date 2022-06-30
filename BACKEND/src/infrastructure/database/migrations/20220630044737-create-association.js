13dab3cffbc8ff8f41183b7f059f55beb1ddb7c0
Rafael Garnasci <rafael.garnasci@gmail.com>
2022-06-30 17:20:03 -0300

    feat(database): relacionamento das migrations

--- BACKEND/src/infrastructure/database/migrations/20220630044737-create-association.js
+++ BACKEND/src/infrastructure/database/migrations/20220630044737-create-association.js
@@ -18,6 +18,57 @@ module.exports = {
         field: 'id_product_size'
       }
     })
+    queryInterface.addConstraint('products', {
+      fields: ['id_product_color'],
+      type: 'foreign key',
+      references: {
+        table: 'product_colors',
+        field: 'id_product_color'
+      }
+    })
+    queryInterface.addConstraint('products', {
+      fields: ['id_product_collection'],
+      type: 'foreign key',
+      references: {
+        table: 'product_collections',
+        field: 'id_product_collection'
+      }
+    })
+    queryInterface.addConstraint('products', {
+      fields: ['id_highlight'],
+      type: 'foreign key',
+      references: {
+        table: 'highlights',
+        field: 'id_highlight'
+      }
+    })
+    queryInterface.addConstraint('products', {
+      fields: ['id_rating'],
+      type: 'foreign key',
+      references: {
+        table: 'ratings',
+        field: 'id_rating'
+      }
+    })
+
+    queryInterface.addConstraint('order_details', {
+      fields: ['id_order'],
+      type: 'foreign key',
+      references: {
+        table: 'orders',
+        field: 'id_order'
+      }
+    })
+    queryInterface.addConstraint('order_details', {
+      fields: ['id_product'],
+      type: 'foreign key',
+      references: {
+        table: 'products',
+        field: 'id_product'
+      }
+    })
+
+
   },
 
   async down (queryInterface, Sequelize) {

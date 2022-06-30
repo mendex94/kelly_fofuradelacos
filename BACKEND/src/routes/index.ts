9fcfa045086003ddd9bcf785a32b80a7bc32274a
tadlana <100472925+tadlana@users.noreply.github.com>
2022-06-30 11:45:59 -0300

    feat(controllers): Criando controller.product

--- BACKEND/src/routes/index.ts
+++ BACKEND/src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ProductsController } from '../domain/product/controllers/product.controller';
 
+
 const routes = express.Router();
 
 routes.get("/catalogo", ProductsController.getAll);

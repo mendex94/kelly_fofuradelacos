6e4945b03e0a7d00fea986c34d7f06758f892029
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-07-02 15:57:32 -0300

    feat: adicionado importação de highlights

--- BACKEND/src/domain/product/services/ProductService.js
+++ BACKEND/src/domain/product/services/ProductService.js
@@ -1,26 +1,26 @@
-const Colors = require('../models/color');
-const { Materials } = require('../models/material')
-const { Products } = require('../models/product');
-const Product_Albums = require('../models/product_album');
-const Sizes = require('../models/size');
+const Colors = require("../models/color");
+const Materials = require("../models/material");
+const Products = require("../models/product");
+const Product_Albums = require("../models/product_album");
+const Sizes = require("../models/size");
+const Highlights = require("../models/highlight");
 
 const ProductService = {
-
   async allProducts() {
     try {
       const fullProducts = await Products.findAll({
         include: [
           {
             model: Materials,
-            as: 'materials',
+            as: "materials",
             through: { attributes: [] },
-          }
-        ]
+          },
+        ],
       });
 
       return fullProducts;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
 
@@ -28,47 +28,61 @@ const ProductService = {
     try {
       const product = await Products.findAll({
         where: { type: type },
-        include: [{
-          model: Product_Albums,
-          required: true
-        },
-        {
-          model: Colors,
-          required: true
-        },
-        {
-          model: Sizes,
-          required: true
-        },
-        {
-          model: Materials,
-          required: true
-        }
+        include: [
+          {
+            model: Product_Albums,
+            required: true,
+          },
+          {
+            model: Colors,
+            required: true,
+          },
+          {
+            model: Sizes,
+            required: true,
+          },
+          {
+            model: Materials,
+            required: true,
+          },
         ],
       });
       return product;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
 
-  async findHighlights(){
-    try{
+  async findHighlights() {
+    try {
       const dataHighlights = await Products.findAll({
         include: [
           {
             model: Highlights,
             as: "Highlights",
             required: true,
-            through: 
-            { attributes: [] },
+            through: { attributes: [] },
+          },
+          {
+            model: Product_Albums,
+            required: true,
+          },
+          {
+            model: Colors,
+            required: true,
+          },
+          {
+            model: Sizes,
+            required: true,
+          },
+          {
+            model: Materials,
+            required: true,
           },
         ],
-      })
-    }catch(error){
-
-    }
-  }
+      });
+    } catch (error) {}
+  },
 };
 
 module.exports = ProductService;

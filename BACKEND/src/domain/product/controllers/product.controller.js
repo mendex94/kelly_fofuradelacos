1b574a39798d121c8bb18091820316a649dc18d1
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-07-02 02:07:41 -0300

    feat: Correção no controler de produtos

--- BACKEND/src/domain/product/controllers/product.controller.js
+++ BACKEND/src/domain/product/controllers/product.controller.js
@@ -1,58 +1,54 @@
-const { Request, Response } = require('express');
-const productService = require('../services')
+const { Request, Response } = require("express");
+const productService = require("../services");
 
 const ProductsController = {
-
   async getAll(req, res) {
     try {
       const products = await productService.allProducts();
       return res.status(200).json(products);
     } catch (error) {
-
       return res.status(500).json(error);
     }
   },
 
-  async getHighlights(req,res){
-    try{
+  async getHighlights(req, res) {
+    try {
       const highlights = await productService.findHighlights();
-      
-      if(!highlights.length > 0){
-        return res.status(400).json("Não existem produtos em destaque")
-      }
-      return res.status(200).json(highlights)
 
-    }catch(error){
-      return res.status(500).json("Erro não catalogado!")
+      if (!highlights.length > 0) {
+        return res.status(400).json("Não existem produtos em destaque");
+      }
+      return res.status(200).json(highlights);
+    } catch (error) {
+      return res.status(500).json("Erro não catalogado!");
     }
   },
 
-async getLacos (req, res) {
-  try {
-    const type_product = 'laços'
-    const lacos = await productService.getProductbyType(type_product);
-    if (!lacos.length > 0) {
-      return res.status(400).json("Não foi encontrado laços");
+  async getLacos(req, res) {
+    try {
+      const type_product = "laços";
+      const lacos = await productService.getProductbyType(type_product);
+      if (!lacos.length > 0) {
+        return res.status(400).json("Não foi encontrado laços");
+      }
+      return res.status(200).json(lacos);
+    } catch (error) {
+      return res.status(500).json(error);
     }
-    return res.status(200).json(lacos);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-},
+  },
 
-async getColecaoParis(req, res) {
-  try {
-    const type_product = 'boina'
-    const colecaoParis = await productService.getProductbyType(type_product);
-    if (!colecaoParis.length > 0) {
-      return res.status(400).json("Não foi encontrado Coleção Paris");
+  async getColecaoParis(req, res) {
+    try {
+      const type_product = "boina";
+      const colecaoParis = await productService.getProductbyType(type_product);
+      if (!colecaoParis.length > 0) {
+        return res.status(400).json("Não foi encontrado Coleção Paris");
+      }
+      return res.status(200).json(colecaoParis);
+    } catch (error) {
+      return res.status(500).json(error);
     }
-    return res.status(200).json(colecaoParis);
-  } catch (error) {
+  },
+};
 
-    return res.status(500).json(error);
-  }
-}
-}
-
-module.exports = ProductsController
+module.exports = ProductsController;

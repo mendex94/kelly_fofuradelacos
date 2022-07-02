dcdf7b3a601c84be900ef9dfa2d905caed3ab5ab
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-07-02 01:50:18 -0300

    feat: Adicionado rota de destaques de produtos

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const ProductsController = require('../domain/product/controllers/product.controller')
+const ProductsController = require('../domain/product/controllers/product.controller');
+const ProductService = require('../domain/product/services/ProductService');
 
 
 const routes = express.Router();
 
 routes.get("/catalogo", ProductsController.getAll);
+
+routes.get("/catalogo/destaques", ProductsController.getHighlights)
 
 module.exports = routes;

75f948090f7dfe02b1a520fd256c0c334c4b42e2
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-07-02 01:49:38 -0300

    feat: correção variavel de ambiente

--- BACKEND/src/infrastructure/database/config/config.js
+++ BACKEND/src/infrastructure/database/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME_TEST,
+    "database": process.env.DB_NAME_DEV,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },

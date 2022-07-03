bb399e8bfb1fe13f691a27e8b644e137f9a7273b
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-07-03 20:07:22 -0300

    fix: Correção nos metodos de busca do banco de dados

--- BACKEND/src/domain/product/services/ProductService.js
+++ BACKEND/src/domain/product/services/ProductService.js
@@ -1,43 +1,41 @@
-const {Colors} = require("../models");
-const {Materials} = require("../models");
-const {Products} = require("../models");
-const {Product_Albums} = require("../models");
-const {Sizes} = require("../models");
-const {Highlights} = require("../models");
+const { Colors } = require("../models");
+const { Materials } = require("../models");
+const { Products } = require("../models");
+const { Product_Albums } = require("../models");
+const { Sizes } = require("../models");
+const { Highlights } = require("../models");
 
 const ProductService = {
-
   async allProducts() {
     try {
       const fullProducts = await Products.findAll();
 
       return fullProducts;
-
     } catch (error) {
       console.log(error);
     }
   },
 
-  async findProductbyType(type) {
+  async findProductbyType(type_product) {
     try {
       const product = await Products.findAll({
-        where: { type: type },
+        where: { type: type_product },
         include: [
           {
             model: Product_Albums,
-            required: true,
+            required: false,
           },
           {
             model: Colors,
-            required: true,
+            required: false,
           },
           {
             model: Sizes,
-            required: true,
+            required: false,
           },
           {
             model: Materials,
-            required: true,
+            required: false,
           },
         ],
       });
@@ -47,36 +45,66 @@ const ProductService = {
     }
   },
 
-  async findHighlights() {
+  async findProductbyId(id) {
+    try {
+      const product = await Products.findAll({
+        where: { id: id },
+        include: [
+          {
+            model: Product_Albums,
+            required: false,
+          },
+          {
+            model: Colors,
+            required: false,
+          },
+          {
+            model: Sizes,
+            required: false,
+          },
+          {
+            model: Materials,
+            required: false,
+          },
+        ],
+      });
+      return product;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async findHighlights(res) {
     try {
       const dataHighlights = await Products.findAll({
         include: [
           {
-            model: Highlights,
-            as: "Highlights",
-            required: true
-          },
-          {
             model: Product_Albums,
-            as: "Album",
-            required: true,
+            required: false,
           },
           {
             model: Colors,
-            required: true,
+            required: false,
           },
           {
             model: Sizes,
-            required: true,
+            required: false,
           },
           {
             model: Materials,
+            required: false,
+          },
+          {
+            model: Highlights,
             required: true,
           },
         ],
       });
-      return dataHighlights
-    } catch (error) {} 
+
+      return dataHighlights;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 

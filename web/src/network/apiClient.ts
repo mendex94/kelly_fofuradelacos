1751d95fff327364885c92fe88b26f4579529a6c
andersonmsousa <anderson.msousa13@gmail.com>
2022-07-07 14:18:23 -0300

    fix: api and forms

--- web/src/network/apiClient.ts
+++ web/src/network/apiClient.ts
@@ -4,7 +4,7 @@ import { Product } from '../@types/products'
 import { Contact } from '../@types/contact'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:4000'
 })
 
 export const renderLacos = ():Promise<Product[]> => {
@@ -13,7 +13,8 @@ export const renderLacos = ():Promise<Product[]> => {
 
 export const postContact = async (contact: Omit<Contact, 'id'>): Promise<Contact> => {
     try {
-        return await api.post("/personalize", contact)
+        const response = await api.post("/personalizado", contact)
+        return response.data
     } catch (error) {
         return error as unknown as Contact;
     }

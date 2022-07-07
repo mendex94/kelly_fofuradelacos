fbf8b05d5391519781cc264a102bb0fe18dd8819
tadlana <100472925+tadlana@users.noreply.github.com>
2022-07-06 23:13:58 -0300

    feat: correção do arquivo rotas

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const OrdersController = require('../domain/order/controllers/order.controller');
 const ProductsController = require('../domain/product/controllers/product.controller');
-// const PersonalizeController = require('../domain/personalize/controllers/personalize.controller');
-// const { PersonalizeValidation } = require ("../domain/personalize/validations/index");
+const PersonalizeController = require('../domain/personalize/controllers/personalize.controller');
+const { PersonalizeValidation } = require ("../domain/personalize/validations/index");
 const OrderValidation = require("../domain/order/validations/order/create");
 
 const routes = express.Router();
 
 routes.get("/catalogo", ProductsController.getAll);
-//routes.get("/catalogo/:id", ProductsController.getProductById);
+routes.get("/catalogo/:id", ProductsController.getProductById);
 
 routes.get("/catalogo/destaques", ProductsController.getHighlights);
 routes.get("/catalogo/lacos", ProductsController.getLacos);

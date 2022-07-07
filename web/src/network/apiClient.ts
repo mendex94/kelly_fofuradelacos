9edbd18ff30501a94d089037bdfc23e81242ce42
andersonmsousa <anderson.msousa13@gmail.com>
2022-07-06 22:55:55 -0300

    adding forms

--- web/src/network/apiClient.ts
+++ web/src/network/apiClient.ts
@@ -1,5 +1,7 @@
+import { BatterySaverOutlined } from '@mui/icons-material'
 import axios from 'axios'
 import { Product } from '../@types/products'
+import { Contact } from '../@types/contact'
 
 const api = axios.create({
     baseURL: 'http://localhost:3333'
@@ -7,4 +9,12 @@ const api = axios.create({
 
 export const renderLacos = ():Promise<Product[]> => {
     return api.get<Product[]>('/lacos').then(response => response.data)    
+}
+
+export const postContact = async (contact: Omit<Contact, 'id'>): Promise<Contact> => {
+    try {
+        return await api.post("/personalize", contact)
+    } catch (error) {
+        return error as unknown as Contact;
+    }
 }
--- web/src/pages/Support/index.tsx
+++ web/src/pages/Support/index.tsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
+import ContactForm from "../../components/ContactFormSupport"
 
 function Support() {
   return (
     <div>
       <Header />
+      <ContactForm />
       <Footer />
     </div>
   )

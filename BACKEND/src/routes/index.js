f57b49d53f0fd47ba2b9d83750bc358dfce47a9e
danilosalys <danilo.pereira.sales@gmail.com>
2022-07-09 01:56:22 -0300

    feat: correção no arquivo de rotas

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -6,9 +6,9 @@ const personalizeValidation = require('../domain/personalize/validations/create'
 const OrderValidation = require("../domain/order/validations/order/create");
 const ProductValidation = require("../domain/product/validations/getone");
 const FreteController = require('../domain/shipping-calculator/controllers/frete.controller');
-const { postQuestion } = require('../domain/question/controllers/question.controller');
 const QuestionController = require('../domain/question/controllers/question.controller');
 const QuestionValidation = require('../domain/question/validations')
+const shippingValidation = require('../domain/shipping-calculator/validations/post')
 
 const routes = express.Router();
 
@@ -21,9 +21,9 @@ routes.get("/catalogo/colecaoparis", ProductsController.getColecaoParis);
 routes.get("/catalogo/tiaras", ProductsController.getTiaras);
 routes.get("/catalogo/acessorios", ProductsController.getAcessorios);
 
-//routes.post("/pedido", OrderValidation.create, OrdersController.postOrder);
-routes.post("/pedido", OrdersController.postOrder);
-routes.post("/frete", FreteController.getFrete);
+routes.post("/pedido", OrderValidation.create, OrdersController.postOrder);
+//routes.post("/pedido", OrdersController.postOrder);
+routes.post("/frete",shippingValidation, FreteController.getFrete);
 routes.post("/personalizado", personalizeValidation, PersonalizeController.postPersonalize);
 routes.post("/duvida", QuestionController.postQuestion);
 

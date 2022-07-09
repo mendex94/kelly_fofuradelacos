1accc93dfbb10b72e1ea3d44e4df5fbef5c89d22
Rafael Garnasci <rafael.garnasci@gmail.com>
2022-07-09 16:37:03 -0300

    feat(env): adicinando acesso DB

--- BACKEND/src/infrastructure/database/config/config.js
+++ BACKEND/src/infrastructure/database/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "wb1lxjbrq22dkla6",//process.env.DB_USER,
-    "password": "ub3i9viylivhwaj8",//process.env.DB_PASSWORD,
-    "database": "l4ahhohue53xr2ct",//process.env.DB_NAME_DEV,
-    "host": "y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",//process.env.DB_HOST,
-    "dialect": "mysql" //process.env.DB_DIALECT
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME_DEV,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": process.env.DB_USER,

670a40ed352be31f74fd0433bcff718801b9986e
danilosalys <danilo.pereira.sales@gmail.com>
2022-07-09 01:55:32 -0300

    feat: Alterando valores das constantes para consultar frete

--- BACKEND/src/domain/shipping-calculator/services/FreteService.js
+++ BACKEND/src/domain/shipping-calculator/services/FreteService.js
@@ -10,12 +10,12 @@ const FreteService = {
     const sCepOrigem = BASE_CEP_ORIGEM;
     const nVlPeso = 1;
     const nCdFormato = 1;
-    const nVlComprimento = 15;
-    const nVlAltura = 10;
-    const nVlLargura = 15;
-    const nVlDiametro = 10;
+    const nVlComprimento = 20;
+    const nVlAltura = 20;
+    const nVlLargura = 20;
+    const nVlDiametro = 20;
     try {
-      const frete = await correios.calcPreco({
+      const frete = await correios.calcPrecoPrazo({
         nCdServico,
         sCepOrigem,
         sCepDestino,

5d8d985670215c602210008eb7d492c3c75089ff
danilosalys <danilo.pereira.sales@gmail.com>
2022-07-09 18:16:51 -0300

    Go-Live: Add script start into package.json

--- web/src/store/modules/cart/cartFetch.ts
+++ web/src/store/modules/cart/cartFetch.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://kellylacosetiarasserver.herokuapp.com'
 })
 
 export const checkoutFetch: any = createAsyncThunk(

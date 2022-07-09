4cd715f05cca18b9e0ae05081dcaa6562884666a
danilosalys <danilo.pereira.sales@gmail.com>
2022-07-09 01:53:09 -0300

    feat: Aplicando validação da rota Order

--- BACKEND/src/domain/order/validations/order/create.js
+++ BACKEND/src/domain/order/validations/order/create.js
@@ -1,28 +1,30 @@
 const { validate, Joi } = require("express-validation");
 
 const create = validate({
-  body: Joi.object({  
+  body: Joi.object({
     name: Joi.string().required(),
     phone: Joi.string().required(),
     email: Joi.string().email().required(),
     total_order: Joi.number().required(),
-    discount: Joi.number(),
+    discount: Joi.any(),
     products_quantity: Joi.number().required(),
-    shipping_total: Joi.number().required(),
-    id_product: Joi.number().required(),
-    amount: Joi.number().required(),
-    unit_value: Joi.number().required(),
-    percentage_discount: Joi.number().required(),
-    id_product_size: Joi.string().required(),
-    id_product_color: Joi.string().required(),
-    id_product_material: Joi.string().required(),
+    shipping_total: Joi.number().required(),    
+    Products:Joi.array().items(Joi.object(
+      {
+        id_product: Joi.number().required(),
+        amount: Joi.number().required(),
+        unit_value: Joi.number().required(),
+        percentage_discount: Joi.any(),
+        id_product_size: Joi.any(),
+        id_product_color: Joi.any(),
+        id_product_material: Joi.any(),
+      }
+    )),
   }),
 });
 
 const OrderValidation = {
-  create
+  create,
 };
 
-module.exports = OrderValidation
-
-
+module.exports = OrderValidation;

30882abfafa5f6faf84d3fdf5054a31b200459b3
mendex94 <lfelipecmendes@icloud.com>
2022-07-09 23:23:51 -0300

    fix: changes pre deploy

--- web/src/components/AboutUsGrid/index.tsx
+++ web/src/components/AboutUsGrid/index.tsx
@@ -7,7 +7,7 @@ import Grid05 from '../../assets/grid-5.png'
 function AboutUsGrid() {
   return (
     <div className='bg-bege-100 lg:pb-14 hidden lg:block'> 
-        <div className="container mx-auto flex flex-row gap-14 justify-center">
+        <div className="container mx-auto flex flex-row gap-8 justify-center">
             
         <img className='hover:scale-105 transition-transform' src={Grid01} alt="" />
             <img className='hover:scale-105 transition-transform' src={Grid02} alt="" />
--- web/src/components/MeetKelly/index.tsx
+++ web/src/components/MeetKelly/index.tsx
@@ -8,7 +8,7 @@ function MeetKelly() {
                 <h1 className='text-center text-h1 text-rosa-200 font-semibold md:mb-12'>Conheça a Kelly</h1>
                 <img src={Kelly} alt="Imagem da Kelly" className='mx-auto my-4 w-8/12 md:w-3/12 md:hidden'/>
                 <div className='flex flex-col gap-5 items-center md:justify-between md:flex-row md:items-stretch'>
-                    <div className='px-8 md:w-9/12 max-w-6xl bg-[#FFFFFF] rounded-[30px] flex flex-col gap-9 pb-10 md:mx-0 md:pb-0 relative'>
+                    <div className='px-8 md:w-9/12 max-w-6xl bg-[#FFFFFF] rounded-[30px] flex flex-col justify-center gap-9 md:mx-0 md:pb-0 relative'>
                         <h2 className='font-semibold italic leading-9 text-h4 mt-7'>Olá, meu nome é Kelly!</h2>
                         <p className='leading-9 text-h4'>
                             Sou artesã já faz dez anos. Iniciei com cartonagem, mas depois de um tempo começei a fazer laços e fiquei apaixonada
